f78ee0bff1d6d69b4f5fc3b24a419cd03d892949
Drosrin <72619346+Drosrin@users.noreply.github.com>
2022-10-24 18:33:45 +0800

    Update enemy.h

--- enemy.h
+++ enemy.h
@@ -1,12 +1,26 @@
 #include <string>
 
-class enemy
+class Enemy;
+class Slime;
+
+Slime slime;
+
+class Enemy
 {
 public:
         int id;
         int atk;
         std::string name;
         std::string description;
-        void specialAttack();
+        virtual void specialAttack(){};
 };
 
+class Slime : public Enemy
+{
+public:
+        int id = 1;
+        int atk = 3;
+        std::string name = "Slime";
+        std::string description = "Weak creature.Deal 3 Damage per turn.";
+        void specialAttack(){};
+}
